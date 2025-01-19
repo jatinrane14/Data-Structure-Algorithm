7419d6d099e37bf48d3aef49b596b6f4a0a17fdb
Jatin Rane <jatinrane23@gmail.com>
2025-01-19 22:11:39 +0530

    Array Added

---
+++ array/LargestElement.java
@@ -0,0 +1,17 @@
+public class LargestElement {
+    static int largest(int arr[]){
+        int n = arr.length;
+        int lar = arr[0];
+        for(int i =1;i<n;i++){
+            if (arr[i]>lar) {
+                lar = arr[i];
+            }
+        }
+        return lar;
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6,50,8,9,10};
+        int lar = largest(arr);
+        System.out.println("Largets  Element is: "+lar);
+    }
+}
