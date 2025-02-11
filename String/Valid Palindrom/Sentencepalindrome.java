e4c7a0d48b685963548311689e15b63e5059d07e
Jatin Rane <jatinrane23@gmail.com>
2025-02-11 13:28:32 +0530

    Valid Sentence Palindrom

---
+++ String/Valid Palindrom/Sentencepalindrome.java
@@ -0,0 +1,23 @@
+public class Sentencepalindrome {
+    public static boolean isPalindrom(String s){
+        String arr[] = new String[s.length()];
+        arr = s.split(" ");
+        System.out.println(arr);
+        String newStr = "";
+        for (String elem : arr) {
+            newStr+=elem;
+        }
+        int low=0,high=newStr.length()-1;
+        while (low<high) {
+            if (newStr.charAt(low)!=newStr.charAt(high)) {
+                return false;
+            }
+            low++;
+            high--;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        System.out.println(isPalindrom("a a"));
+    }   
+}
