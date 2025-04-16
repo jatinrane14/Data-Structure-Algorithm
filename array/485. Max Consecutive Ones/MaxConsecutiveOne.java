f40acac78af8487dbfb865181d4ebb2be60de7de
Jatin Rane <jatinrane23@gmail.com>
2025-04-16 22:18:42 +0530

    Add algo Duplicate Eleme

--- array/485. Max Consecutive Ones/MaxConsecutiveOne.java
+++ array/485. Max Consecutive Ones/MaxConsecutiveOne.java
@@ -1,5 +1,5 @@
 class Solution{
-    public static int maxConsecutiveOne(int arr[]){
+    public int maxConsecutiveOne(int arr[]){
         int count =0;
         int maxCount = 0;
         for (int digit : arr) {
@@ -18,6 +18,8 @@ public class MaxConsecutiveOne{
     public static void main(String[] args) {
         Solution s = new Solution();
         int arr[] = {1,1,0,1,1,1,1,1,1,1,1,0,1,1,1};
+        long startTime = System.currentTimeMillis();
+        System.out.println(startTime);
         int res = s.maxConsecutiveOne(arr);
         System.out.println(res);
     }
--- array/RemoveDuplicate/RemoveDuplicate.java
+++ array/RemoveDuplicate/RemoveDuplicate.java
@@ -1,14 +1,21 @@
 import java.util.HashSet;
 import java.util.Set;
+
+/*
+ * 1. Create a HashSet to store unique elements.
+ * 2. Iterate through the array and add each element to the HashSet.
+ * 3. After the loop, the HashSet will contain only unique elements.
+ * 4. Create a new array of the same size as the HashSet.
+ * 5. Iterate through the HashSet and copy each element to the new array.
+ * 6. Return the new array.
+ */
 public class RemoveDuplicate {
     static int removeDuplicate(int[] arr){
         Set<Integer> s = new HashSet<>();
         int index=0;
-        
         for(int i=0;i<arr.length;i++){
             s.add(arr[i]);
         }
-
         for (int it : s) {
             arr[index] = it;
             index++;
@@ -17,7 +24,6 @@ public class RemoveDuplicate {
     }
     public static void main(String args[]){
         int arr[]={1,2,2,3,4,4,5,6};
-        
         System.out.println(removeDuplicate(arr));
     }
 }
