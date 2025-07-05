03e3c12b1707a2e764a13e273e9d0ca3c754832c
Jatin Rane <jatinrane23@gmail.com>
2025-07-05 10:56:04 +0530

    Array: Larget Element(Optimal)

--- array/LargestNumber/LargestElementRecursiveForce.java
+++ array/LargestNumber/LargestElementRecursiveForce.java
@@ -1,13 +1,12 @@
 public class LargestElementRecursiveForce {
     static int largest(int arr[]){
-        int n = arr.length;
-        int lar = arr[0];
-        for(int i =1;i<n;i++){
-            if (arr[i]>lar) {
-                lar = arr[i];
+        int largetElem = arr[0];
+        for(int i =1;i<arr.length;i++){
+            if (largetElem < arr[i]) {
+                largetElem = arr[i];
             }
         }
-        return lar;
+        return largetElem;
     }
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,5,6,50,8,9,10};
