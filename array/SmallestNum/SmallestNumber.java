912e62474ac7412e51420c41f555011eb7c26208
Jatin Rane <jatinrane23@gmail.com>
2025-07-08 11:21:58 +0530

    Array: Smallest Number

--- array/SmallestNum/SmallestNumber.java
+++ array/SmallestNum/SmallestNumber.java
@@ -1,13 +1,13 @@
 public class SmallestNumber {
     public static void main(String[] args) {
-        int arr[] = {12,3,6,17,40,5};
+        int arr[] = {12,3,-1,17,40,5};
         System.out.println(smallestNum(arr));
     }
 
     static int smallestNum(int arr[]){
-        int  smallest = Integer.MAX_VALUE;
-        for(int i=0;i<arr.length;i++){
-            if (arr[i]<smallest) {
+        int smallest = Integer.MAX_VALUE;
+        for(int i =0;i<arr.length;i++){
+            if(arr[i]<smallest){
                 smallest = arr[i];
             }
         }
