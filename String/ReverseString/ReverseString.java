77d20620a9cbbf13571cb81d581907dbd9d587de
Jatin Rane <jatinrane23@gmail.com>
2025-07-08 23:00:47 +0530

    Reverse String

--- String/ReverseString/ReverseString.java
+++ String/ReverseString/ReverseString.java
@@ -1,12 +1,12 @@
 public class ReverseString{
     public static String reverse(String s){
-        String newStr = "";
-        for(int i =s.length()-1;i>=0;i--){
+        String newStr="";
+        for(int i= s.length()-1;i>=0;i--){
             newStr+=s.charAt(i);
         }
         return newStr;
     }
     public static void main(String[] args) {
-        System.out.println(reverse("121"));
+        System.out.println(reverse("Hello World!"));
     }
 }
