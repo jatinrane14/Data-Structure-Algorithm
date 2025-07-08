d56f6acb7553e2b2b13c12802f1c5ac7c3876c0a
Jatin Rane <jatinrane23@gmail.com>
2025-07-08 23:03:51 +0530

    String: COnvert to lower case

--- String/Convertolower/StringLower.java
+++ String/Convertolower/StringLower.java
@@ -1,15 +1,15 @@
 public class StringLower {
     public static String toLower(String st){
-        String newStr="";
+        StringBuilder newStr = new StringBuilder();
         for(int i =0;i<st.length();i++){
             if (st.charAt(i)>=65 && st.charAt(i)<=90) {
-                newStr+=(char)(st.charAt(i)+32);
+                newStr.append((char)(st.charAt(i)+32));
             }
             else{
-                newStr+=st.charAt(i);
+                newStr.append(st.charAt(i));
             }
         }
-        return newStr;
+        return newStr.toString();
     }
     public static void main(String[] args) {
         System.out.println(toLower("HELLO BHAI"));
