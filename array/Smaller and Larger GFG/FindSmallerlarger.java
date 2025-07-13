79793999fc2104e5cb5782d69e168becafcd3556
Jatin Rane <jatinrane23@gmail.com>
2025-07-13 11:29:20 +0530

    Array(reapeated): Left Rotete by One

--- array/Smaller and Larger GFG/FindSmallerlarger.java
+++ array/Smaller and Larger GFG/FindSmallerlarger.java
@@ -1,3 +1,5 @@
+//Q. Given a sorted array arr of non-repeating elements and a value target, return an array of size 2. The first value is the number of elements less than or equal to the target, and the second value is the number of elements greater than or equal to the target.
+
 import java.util.Arrays;
 public class FindSmallerlarger {
     public static int binarySearch(int arr[],int low,int high,int target){
@@ -21,7 +23,7 @@ public class FindSmallerlarger {
         return new int[]{smaller,greator};
     }
     public static void main(String[] args) {
-        int arr[] ={1, 5, 8, 11, 12, 18, 19};
+        int arr[] ={1, 10,21, 8, 11, 12, 18, 19};
         int target = 12;
         System.out.println(Arrays.toString(findSolution(arr, target)));
     } 
