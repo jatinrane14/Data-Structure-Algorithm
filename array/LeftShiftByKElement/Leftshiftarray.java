f791e470b373f2d44eff78892f58bc834d49580b
Jatin Rane <jatinrane23@gmail.com>
2025-07-14 12:46:27 +0530

    Arrays: Left shift by k places

--- array/LeftShiftByKElement/Leftshiftarray.java
+++ array/LeftShiftByKElement/Leftshiftarray.java
@@ -1,26 +1,38 @@
 import java.util.Arrays;
 
 public class Leftshiftarray {
-    public static void main(String[] args) {
-        int arr[] = { 1, 2, 3, 4, 5,6,7};
-        int k=3;
-        if(k>arr.length){
-            k = k%arr.length;
+    static int[] shifting(int arr[],int k){
+        int n = arr.length;
+        if(k==n){
+            return arr;
         }
+        if(k>n){
+            k = k%n;
+        }
+        // Store in temp array
         int temp[] = new int[k];
-        for(int i=0;i<k;i++){
+        for(int i =0;i<k;i++){
             temp[i] = arr[i];
         }
-        System.out.println("Temp: "+Arrays.toString(temp));
-        for(int i = k;i<arr.length;i++){
-            arr[i - k] = arr[i];
+        
+        // shfting
+        for(int i = k;i<n;i++){
+            arr[i-k] = arr[i];
         }
-        System.out.println("Only rotate: "+Arrays.toString(arr));
-        int j=0;
-        for(int i=arr.length-k;i<arr.length;i++){
+
+        // Place temp array elem at last in array
+        int j =0;
+        for(int i = n-k; i<n;i++){
             arr[i] = temp[j];
             j++;
         }
-        System.out.println(Arrays.toString(arr));
+
+        return arr;
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6,7,8,9};
+        int k = 9;
+        System.out.println(Arrays.toString(shifting(arr,k)));
+        
     }
 }
