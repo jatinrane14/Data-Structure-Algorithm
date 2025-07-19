6675dc1c048744181201b46c3252499f05d877a0
Jatin Rane <jatinrane23@gmail.com>
2025-07-19 10:38:03 +0530

    Aarrays: Move zero at last(Brute Force)

--- array/MoveAllZeroLast/Moveatlast.java
+++ array/MoveAllZeroLast/Moveatlast.java
@@ -3,17 +3,14 @@ import java.util.Arrays;
 import java.util.List;
 public class Moveatlast {
     public static void main(String[] args) {
-
-        // Brute force approach
-
-        int arr[]={1,0,2,8,3,2,0,0,8};
-        List<Integer> li = new ArrayList<Integer>();
-        for (int i=0;i<arr.length;i++) {
+        int arr[] ={1,0,0,2,0,4,7,0,4,0};
+        List<Integer> li = new ArrayList<>();
+        for(int i =0;i<arr.length;i++){
             if(arr[i]!=0){
                 li.add(arr[i]);
             }
         }
-        for(int i = 0;i<arr.length;i++){
+        for(int i =0;i<arr.length;i++){
             if(i<li.size()){
                 arr[i] = li.get(i);
             }
@@ -22,9 +19,7 @@ public class Moveatlast {
             }
         }
         System.out.println(Arrays.toString(arr));
-
-        // T.C. -> O(n)
-        // S.C. -> O(n)
+        
     }    
 
 }
