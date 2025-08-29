e1d42ebaf11252ba9fb3735c0bd83352289ac4d4
SVVV Campus Scout <campusscout@svvv.edu.in>
2025-08-29 21:40:46 +0530

    Graph representation

---
+++ Graph/graphrepresentation.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class graphrepresentation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no. of vertices: ");
+        int v = sc.nextInt();
+        System.out.println("Enter no. of edges: ");
+        int e = sc.nextInt();
+        int[][] adj = new int[v+1][v+1];   
+        for (int i = 0; i < e; i++) {
+            System.out.println("Enter edge (From): ");
+            int v1 = sc.nextInt();
+            System.out.println("Enter edge (To): ");
+            int v2 = sc.nextInt();
+            adj[v1][v2] = 1;
+            adj[v2][v1] = 1;
+        }
+
+        System.out.println("\nAdjacency Matrix:");
+        for (int i = 1; i <= v; i++) {
+            for (int j = 1; j <= v; j++) {
+                System.out.print(adj[i][j] + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
